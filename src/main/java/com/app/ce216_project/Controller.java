95ed475efea0d051adcd3d316ab95a1732e11a6e
Onur Öztop <onureldrinx@gmail.com>
2022-04-25 23:20:09 +0300

    UI Improvement

--- src/main/java/com/app/ce216_project/Controller.java
+++ src/main/java/com/app/ce216_project/Controller.java
@@ -8,15 +8,18 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.TextFieldTreeCell;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.*;
 
 public class Controller implements Initializable {
+
 
     ObservableList<Type> typeList2 = FXCollections.observableArrayList();
     ArrayList<Type> typeList = new ArrayList<>();
@@ -66,28 +69,24 @@ public class Controller implements Initializable {
     private Scene currentScene;
 
     @FXML
-    private ChoiceBox typeChoice;
+    private ChoiceBox<Object> typeChoice;
 
     @FXML
     private TextArea textArea;
-
-    int counter =0;
-
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         TreeItem<Object> treeRoot = new TreeItem<>("Types");
         System.out.println(Catalog.getCatalogInstance().getTypes().size());
         tree.setRoot(treeRoot);
-
-
         typeChoice.setValue("Types");
         typeChoice.getItems().addAll(typeList);
 
     }
 
-    public void choiceBoxRefresh(){
-        typeChoice.getItems().addAll(typeList);
+
+    public void choiceBoxRefresh(Type newType){
+        typeChoice.getItems().add(newType);
     }
 
 
@@ -136,10 +135,12 @@ public class Controller implements Initializable {
         tree.getRoot().getChildren().add(treeItem);
         System.out.println(tree.getRoot().getChildren().size());
 
+
+
         //createWindow.setVisible(false);
         addAttributeWindow.setVisible(true);
         typeList.add(t);
-        choiceBoxRefresh();
+        choiceBoxRefresh(t);
         createWindow.setVisible(false);
 
     }
@@ -197,7 +198,7 @@ public class Controller implements Initializable {
 
             System.out.println("Item is null : " +  (item == null));
 
-            counter++;
+
 
 
             if(item != tree.getRoot()){
